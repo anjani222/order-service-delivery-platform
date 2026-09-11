860205de32575693b9a73f47bc225206684c5099
Anjani Jha <anjanikumarjha1@gmail.com>
2026-09-11 18:04:38 +0000

    enable restricted GitHub webhook access

--- infra/terraform/jenkins.tf
+++ infra/terraform/jenkins.tf
@@ -32,6 +32,14 @@ resource "aws_security_group" "jenkins" {
   }
 
   ingress {
+    description = "GitHub webhook delivery"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["192.30.252.0/22", "185.199.108.0/22", "140.82.112.0/20", "143.55.64.0/20"]
+  }
+
+  ingress {
     description = "SonarQube web interface"
     from_port   = 9000
     to_port     = 9000
